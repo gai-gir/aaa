a530984de552d66393a7bd0474d00c9e4abea8f6
Alina <Alina@mail.ru>
2016-10-17 14:54:29 +0300

    secondSort

---
+++ SecondMethod.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+public class SecondMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final Random random = new Random();
+		int[] array;
+		array = new int[random.nextInt(1000)];
+		for (int i = 0; i < array.length; i++) {
+		    array[i] = ((int)(Math.random() * random.nextInt()));
+		    System.out.print(array[i]+" ");
+		}
+		System.out.println("");
+		for(int i = array.length-1;i>0;i--)	{
+			for(int j = 0;j<=i;j++){
+				if (j==i){
+					for (;j<0;j--){
+						if(array[j]<array[j-1]){
+							int s = array[j-1];
+							array[j-1] = array[j];
+							array[j] = s;
+						}
+					}
+					break;
+				}
+				if(array[j]>array[j+1]){
+					int p = array[j];
+					array[j]=array[j+1];
+					array[j+1]=p;
+					
+				}	
+			}
+		}
+		for(int i = 0;i<array.length;i++){
+			 System.out.print(array[i]+" ");
+		}
+	}
+
+}
